56187fb231063f80e11b3c3d3aa7fc3d645ef369
Lyubane4ka <lyubane4ka@gmail.com>
2025-09-05 15:30:57 +0300

    add O(n)

--- Task19.py
+++ Task19.py
@@ -4,11 +4,13 @@ def print_students_info(students):
     :param students: (list) Список словарей с информацией о студентах
     :return: students info
     """
-    if students is not None:
-        for student in students:
-            name = student.get('имя', 'Не указано')
-            age = student.get('возраст', 'Не указан')
-            course = student.get('курс', 'Не указан')
-            print(f"Имя: {name}, Возраст: {age}, Курс: {course}")
+    if students is not None:  # O(1)
+        for student in students:  # O(n)
+            name = student.get('имя', 'Не указано')  # O(1) - доступ по ключу в словаре
+            age = student.get('возраст', 'Не указан')  # O(1)
+            course = student.get('курс', 'Не указан')  # O(1)
+            print(f"Имя: {name}, Возраст: {age}, Курс: {course}")  # O(1)-вывод 1 строки
         else:
-            print("Данные отсутствуют")
+            print("Данные отсутствуют")  # в случае выполнения O(1)
+
+# общая временная сложность алгоритма O(n)
